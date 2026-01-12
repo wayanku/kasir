739d0e789a06c014b46f501d3ee893e35eb37296
Storybali <sudiarsanachannel@gmail.com>
2026-01-12 21:31:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'kasir-v2';
+const CACHE_NAME = 'v12_kasir';
 const ASSETS = [
   'index.html',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/html5-qrcode'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
